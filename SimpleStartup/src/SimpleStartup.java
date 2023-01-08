09be69a33cf31c1c9f4e208b5ddcbd7d96d75265
William Lac <me@williamlac.com>
2023-01-07 17:17:52 -0800

    simple startup test

--- SimpleStartup/src/SimpleStartup.java
+++ SimpleStartup/src/SimpleStartup.java
@@ -1,5 +1,27 @@
-public class SimpleStartup {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+class SimpleStartup {
+    // first instance variables
+    private int[] locationCells;
+    private int numOfHits = 0;
+
+    // methods
+    public void setLocationCells(int[] locs) {
+        locationCells = locs;
     }
-}
+
+    public String checkYourself(int guess) {
+        String result = "miss";
+        for (int cell : locationCells) {
+            if (guess == cell) {
+                result = "hit";
+                numOfHits++;
+                break;
+            } // end if
+        } //end for
+        if (numOfHits == locationCells.length) {
+            result = "kill";
+        } // end if
+        System.out.println(result);
+        return result;
+    }
+} // close SimpleStartup Class
+
