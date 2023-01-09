2c2e3f016b8f2f120c16cf970790b0fcc8f8e06f
William Lac <wlac217@gmail.com>
2023-01-09 14:21:24 -0800

    init startupbust

--- SimpleStartup/src/Startup.java
+++ SimpleStartup/src/Startup.java
@@ -1,27 +1,46 @@
-class Startup {
+import java.util.ArrayList;
+
+public class Startup {
     // first instance variables
-    private int[] locationCells;
-    private int numOfHits = 0;
+    private ArrayList<String> locationCells;
+    // private int numOfHits = 0;
+    // do not need numOfHits anymore as we're using ArrayList
 
     // methods
-    public void setLocationCells(int[] locs) {
+    public void setLocationCells(ArrayList<String> locs) {
         locationCells = locs;
     }
 
-    public String checkYourself(int guess) {
+    public String checkYourself(String userInput) {
         String result = "miss";
-        for (int cell : locationCells) {
-            if (guess == cell) {
+
+        int index = locationCells.indexOf(userInput);
+
+        if (index >= 0) {
+            locationCells.remove(index);
+
+            if (locationCells.isEmpty()) {
+                result = "kill";
+            } else {
                 result = "hit";
-                numOfHits++;
-                break;
-            } // end if
-        } //end for
-        if (numOfHits == locationCells.length) {
-            result = "kill";
-        } // end if
-        System.out.println(result);
+            }
+        }
         return result;
     }
-} // close SimpleStartup Class
+}
+
+//        for (int cell : locationCells) {
+//            if (guess == cell) {
+//                result = "hit";
+//                numOfHits++;
+//                break;
+//            } // end if
+//        } //end for
+//        if (numOfHits == locationCells.length) {
+//            result = "kill";
+//        } // end if
+//        System.out.println(result);
+//        return result;
+//    }
+//} // close SimpleStartup Class
 
