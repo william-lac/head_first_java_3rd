39569350b21bb516b919f92e28044411084eae94
William Lac <wlac217@gmail.com>
2023-01-09 14:50:40 -0800

    completed startupbust

--- StartupBust/src/StartupBust.java
+++ StartupBust/src/StartupBust.java
@@ -1,3 +1,36 @@
+import java.util.ArrayList;
 public class StartupBust {
+    private GameHelper helper = new GameHelper();
+    private ArrayList<Startup> startups = new ArrayList<Startup>();
+    private int numOfGuesses = 0;
 
+    private void setUpGame() {
+        // first make some Startups and give them locations
+        Startup one = new Startup();
+        one.setName("pionize");
+        Startup two = new Startup();
+        two.setName("hacqi");
+        Startup three = new Startup();
+        three.setName("cabista");
+        startups.add(one);
+        startups.add(two);
+        startups.add(three);
+
+        System.out.println("Your goal is to sink three Starups.");
+        System.out.println("poniez, hacqi, cabista");
+        System.out.println("Try to sink them all in the fewest number of guesses");
+
+        for (startup startup : startups) {
+            ArrayList<String> newLocation = helper.placeStartup(3);
+            startup.setLocationCells(newLocation);
+        } // close for loop
+    } //close setUpGame method
+
+    private void startPlaying() {
+        while (!startups.isEmpty()) {
+            String userGuess = helper.getUserInput("Enter a guess");
+            checkUserGuess(userGuess);
+        } // close while
+        finishGame();
+    } // close startPlaying method
 }
