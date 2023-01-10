e41c4f7812920b9adadb3aeedf427c7f99ac9181
William Lac <wlac217@gmail.com>
2023-01-09 17:53:37 -0800

    added more methods to startupbust

--- StartupBust/src/StartupBust.java
+++ StartupBust/src/StartupBust.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
 public class StartupBust {
+    // Declare and initialize the variables we'll need
     private GameHelper helper = new GameHelper();
     private ArrayList<Startup> startups = new ArrayList<Startup>();
     private int numOfGuesses = 0;
 
     private void setUpGame() {
         // first make some Startups and give them locations
+        // Make three Startup objects, give 'em names, and stick 'em in the Arraylist
         Startup one = new Startup();
         one.setName("pionize");
         Startup two = new Startup();
@@ -16,6 +18,7 @@ public class StartupBust {
         startups.add(two);
         startups.add(three);
 
+        // print brief instructions for player
         System.out.println("Your goal is to sink three Starups.");
         System.out.println("poniez, hacqi, cabista");
         System.out.println("Try to sink them all in the fewest number of guesses");
@@ -31,6 +34,42 @@ public class StartupBust {
             String userGuess = helper.getUserInput("Enter a guess");
             checkUserGuess(userGuess);
         } // close while
+        // call our own finishg\Game method
         finishGame();
     } // close startPlaying method
+
+    private void checkUserGuess(String userGuess) {
+        numOfGuesses++;
+        String result = "miss";
+
+        for (Startup startupToTest : startups) {
+            result = startupToTest.checkYourself(userGuess);
+
+            if (result.equals("hit")) {
+                break;
+            }
+            if (result.equals("kill")) {
+                startups.remove(startupToTest);
+                break;
+            }
+        } // close for
+    } // close method
+
+    private void finishGame() {
+        System.out.println("All Startups are dead!  Your stock is now worthless");
+        if (numOfGuesses <= 18) {
+            System.out.println("It only took you " + numOfGuesses + " guesses.");
+            System.out.println("You got out before your options sank.");
+            System.out.println("Took you long enough. " + numOfGuesses + " guesses.");
+            System.out.println("Fish are dancing with your options.");
+        }
+    } // close method
+
+    // main method setup and starts the game
+    public static void main(String[] args) {
+        StartupBust game = new StartupBust();
+        game.setUpGame();
+        game.startPlaying();
+    } // close method
+
 }
