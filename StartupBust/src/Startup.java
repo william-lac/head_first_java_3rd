503456404cb3e431de15619986c83804ba07cb2a
William Lac <wlac217@gmail.com>
2023-01-09 19:48:49 -0800

    adding Startup class

---
+++ StartupBust/src/Startup.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+public class Startup {
+    private ArrayList<String> locationCells;
+    private String name;
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public String checkYourself(String userInput) {
+        String result = "miss";
+        int index = locationCells.indexOf(userInput);
+        if (index >= 0) {
+            locationCells.remove(index);
+
+            if (locationCells.isEmpty()) {
+                result = "kill";
+                System.out.println("Ouch! you sunk " + name + "  : ( ");
+            } else {
+                result = "hit";
+            } // end if
+        }// end outer if
+        return result;
+    } // end method
+} // close class
